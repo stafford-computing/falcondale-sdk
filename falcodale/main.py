061936f762a9e49ffc1983e84130d795c5a30e03
Fabio Beider <fabio.beider@altbank.co>
2023-03-30 19:59:55 -0300

    fix: adjust predict

--- falcodale/main.py
+++ falcodale/main.py
@@ -21,6 +21,7 @@ class Falcodale:
               feature_selection_type: str,
               feature_selection_backend: str,
               target_variable: str,
+              csv_data_filename: str,
               csv_data: BufferedReader) -> bool:
 
         self._model_type = model_type
@@ -38,7 +39,7 @@ class Falcodale:
         url += f"&target_variable={self._target_variable}"
 
         r = requests.post(
-            url=url, files={'csv_data': ("csv_data.csv", self._csv_data)})
+            url=url, files={'csv_data': (csv_data_filename, self._csv_data)})
 
         if r.status_code == 200:
             self._score = r.json()[0]
@@ -51,21 +52,39 @@ class Falcodale:
 
             return False
 
+    # def predict(self,
+    #             **kwargs):
+
+    #     if self._trained_file is None:
+    #         print("Please train/load the model first")
+    #         return
+
+    #     binary_data = BytesIO(json.dumps(kwargs).encode())
+
+    #     url = f"{self._api_server_url}/predict"
+    #     url += f"?model_type={self._model_type}"
+    #     url += f"&model_id={self._model_uuid}"
+
+    #     r = requests.post(
+    #         url=url, files={'csv_data': ("csv_data.csv", binary_data)})
+
+    #     return r
+
     def predict(self,
-                **kwargs):
+                model_name: str,
+                csv_data_filename: str,
+                csv_data: BufferedReader):
 
         if self._trained_file is None:
             print("Please train/load the model first")
             return
 
-        binary_data = BytesIO(json.dumps(kwargs).encode())
 
         url = f"{self._api_server_url}/predict"
-        url += f"?model_type={self._model_type}"
-        url += f"&model_id={self._model_uuid}"
+        url += f"?model_name={model_name}"
 
         r = requests.post(
-            url=url, files={'csv_data': ("csv_data.csv", binary_data)})
+            url=url, files={'csv_data': (csv_data_filename, csv_data)})
 
         return r
 
